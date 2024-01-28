67708a33907f51daad36008254b405d1e0d85df2
Chyngyz <142288166+AlmazovChyngyz@users.noreply.github.com>
2024-01-28 23:33:41 +0600

    added fourth homework

---
+++ hw-4/src/components/input/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './input.module.css';
+
+const Input = ({type, placeholder, onChangeInput}) => {
+    return (
+        <input className={classes.inputStyle}
+            type={type}
+            placeholder={placeholder}
+            onChange={onChangeInput}
+        />
+    );
+};
+
+export default Input;
